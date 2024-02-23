ee8cf0b4bd48750011a1068fbb902c8cf83571a7
MarucoMass <mariomass23@gmail.com>
2024-02-23 16:02:23 -0300

    map

--- curso-reactjs/src/Ternario.jsx
+++ curso-reactjs/src/Ternario.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 /* eslint-disable react/prop-types */
 const Items = ({ nombre, visto }) => {
     return (
@@ -10,6 +12,22 @@ const Items = ({ nombre, visto }) => {
 }
 
 export const Ternario = () => {
+
+    const addTask = () => {
+        setArreglo([...arreglo, { nombre: `tarea nueva ${arreglo.length+1}`, visto: true }])
+    }
+
+    let listadoSecciones = [
+        { nombre: 'Props', visto: true },
+        { nombre: 'Eventos', visto: true },
+        { nombre: 'useState', visto: true },
+        { nombre: 'Ternario', visto: true },
+        { nombre: 'Redux', visto: false },
+        { nombre: 'useContext', visto: false },
+    ]
+
+    const [arreglo, setArreglo] = useState(listadoSecciones)
+
     return (
         <>
             <h1>Ternario</h1>
@@ -18,6 +36,15 @@ export const Ternario = () => {
                 <Items nombre='Eventos' visto={true} />
                 <Items nombre='Redux' visto={false} />
             </ol>
+            <h1>Map</h1>
+            <ol>
+                {
+                    arreglo.map((item, index) =>
+                        <Items key={index} nombre={item.nombre} visto={item.visto} />
+                        )
+                }
+            </ol>
+            <button onClick={addTask}>Agregar tarea</button>
         </>
     )
 }
